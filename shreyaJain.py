e5769b51cea0c92df0eb5d6617301b6191b702fc
Shreya <shreya11072003@gmail.com>
2022-05-20 14:00:45 +0530

    Recommited

--- shreyaJain.py
+++ shreyaJain.py
@@ -9,76 +9,19 @@ while True:
     pyautogui.keyDown('e')
     pyautogui.keyDown('y')
     pyautogui.keyDown('a')
+    pyautogui.keyDown(" ")
     pyautogui.keyDown('J')
     pyautogui.keyDown('a')
     pyautogui.keyDown('i')
     pyautogui.keyDown('n')
     pyautogui.keyDown(" ")
     pyautogui.keyDown(" ")
+    pyautogui.keyDown("\n")
+    pyautogui.keyDown("ctrl+enter")
     pyautogui.keyDown("enter")
     pyautogui.keyDown("enter")
 
-    
-#ShreyaJain  
 
-#ShreyaJain  
 
-#ShreyaJain  
 
-#ShreyaJain  
 
-#ShreyaJain  
-
-#ShreyaJain  
-
-#ShreyaJain  
-
-#ShreyaJain  
-
-#ShreyaJain  
-
-#ShreyaJain  
-
-#ShreyaJain  
-
-#ShreyaJain  
-
-#ShreyaJain  
-
-#ShreyaJain  
-
-#ShreyaJain  
-
-#ShreyaJain  
-
-#ShreyaJain  
-
-#ShreyaJain  
-
-#ShreyaJain  
-
-#ShreyaJain  
-
-#ShreyaJain  
-
-#ShreyaJain  
-
-#ShreyaJain  
-
-#ShreyaJain  
-
-#ShreyaJain  
-
-#ShreyaJain  
-
-#ShreyaJain  
-
-#ShreyaJain  
-
-#ShreyaJain  
-
-#ShreyaJain  
-
-#ShreyaJain  
-
-#ShreyaJain  
