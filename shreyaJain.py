20beff76c6eddd92b35138c5582e1f58b23caa56
Shreya <shreya11072003@gmail.com>
2022-05-20 10:48:14 +0530

    initial commit

---
+++ shreyaJain.py
@@ -0,0 +1,84 @@
+
+import pyautogui
+
+while True:
+    pyautogui.keyDown('#')
+    pyautogui.keyDown('S')
+    pyautogui.keyDown('h')
+    pyautogui.keyDown('r')
+    pyautogui.keyDown('e')
+    pyautogui.keyDown('y')
+    pyautogui.keyDown('a')
+    pyautogui.keyDown('J')
+    pyautogui.keyDown('a')
+    pyautogui.keyDown('i')
+    pyautogui.keyDown('n')
+    pyautogui.keyDown(" ")
+    pyautogui.keyDown(" ")
+    pyautogui.keyDown("enter")
+    pyautogui.keyDown("enter")
+
+    
+#ShreyaJain  
+
+#ShreyaJain  
+
+#ShreyaJain  
+
+#ShreyaJain  
+
+#ShreyaJain  
+
+#ShreyaJain  
+
+#ShreyaJain  
+
+#ShreyaJain  
+
+#ShreyaJain  
+
+#ShreyaJain  
+
+#ShreyaJain  
+
+#ShreyaJain  
+
+#ShreyaJain  
+
+#ShreyaJain  
+
+#ShreyaJain  
+
+#ShreyaJain  
+
+#ShreyaJain  
+
+#ShreyaJain  
+
+#ShreyaJain  
+
+#ShreyaJain  
+
+#ShreyaJain  
+
+#ShreyaJain  
+
+#ShreyaJain  
+
+#ShreyaJain  
+
+#ShreyaJain  
+
+#ShreyaJain  
+
+#ShreyaJain  
+
+#ShreyaJain  
+
+#ShreyaJain  
+
+#ShreyaJain  
+
+#ShreyaJain  
+
+#ShreyaJain  
